dda97efec8273844969caa3519dbbcb97ac8327e
Kengo TODA <skypencil@gmail.com>
2019-01-07 11:15:01 +0800

    format code by spotless

--- src/main/java/jp/skypencil/Main.java
+++ src/main/java/jp/skypencil/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
@@ -34,23 +33,29 @@ public class Main {
       int methodIndex = 0;
       for (MethodNode method : classNode.methods) {
         if (method.instructions.size() > 0) {
-          System.out.printf("  subgraph cluster_method_%d {%n    label=\"#%s\";%n    graph[style=dotted];%n", methodIndex, method.name, method.name);
+          System.out.printf(
+              "  subgraph cluster_method_%d {%n    label=\"#%s\";%n    graph[style=dotted];%n",
+              methodIndex, method.name, method.name);
           final int index = methodIndex;
           for (int i = 0; i < method.instructions.size(); ++i) {
-              AbstractInsnNode insn = method.instructions.get(i);
-              System.out.printf("    edge%d_%d [label=\"%s\"];%n", index, i, insn.getClass());
+            AbstractInsnNode insn = method.instructions.get(i);
+            System.out.printf("    edge%d_%d [label=\"%s\"];%n", index, i, insn.getClass());
           }
 
           Analyzer<BasicValue> analyzer =
               new Analyzer<BasicValue>(new BasicInterpreter()) {
                 @Override
                 protected void newControlFlowEdge(final int insnIndex, final int successorIndex) {
-                  System.out.printf("    edge%d_%d -> edge%d_%d;%n", index, insnIndex, index, successorIndex);
+                  System.out.printf(
+                      "    edge%d_%d -> edge%d_%d;%n", index, insnIndex, index, successorIndex);
                 }
 
                 @Override
-                protected boolean newControlFlowExceptionEdge(final int insnIndex, final int successorIndex) {
-                  System.out.printf("    edge%d_%d -> edge%d_%d [color = \"red\"];%n", index, insnIndex, index, successorIndex);
+                protected boolean newControlFlowExceptionEdge(
+                    final int insnIndex, final int successorIndex) {
+                  System.out.printf(
+                      "    edge%d_%d -> edge%d_%d [color = \"red\"];%n",
+                      index, insnIndex, index, successorIndex);
                   return true;
                 }
               };
